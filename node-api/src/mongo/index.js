c8803e0529f5bffe2f728b8cf764a122b7883d86
Aryan <aryan.bhatnagar2812@gmail.com>
2023-12-05 22:13:35 +0530

    first commit

---
+++ node-api/src/mongo/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const dbConnect = () => {
+  mongoose.connection.once("open", () => console.log("DB connection"));
+  return mongoose.connect(
+    `${process.env.DB_LINK}`,
+    { keepAlive: true }
+  );
+};
